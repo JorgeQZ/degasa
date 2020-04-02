95c7c478529de2e87c6005eba733f794af1f7945
JorgeQZ <jorge_qzg@hotmail.com>
2020-04-02 17:25:29 -0600

    home-full-desktop

--- front-page.php
+++ front-page.php
@@ -154,4 +154,40 @@ get_header();?>
         </div>
     </div>
 </div>
+
+<div class="contact-container">
+    <div class="title">
+        Queremos ser parte <br>
+        de su siguiente proyecto
+    </div>
+    <div class="subtitle">
+        Póngase en contacto con nosotros
+    </div>
+    <div class="form">
+        <div class="col">
+            <div class="form-control">
+                <input type="text" placeholder="NOMBRE">
+            </div>
+            <div class="form-control">
+                <input type="text" placeholder="EMPRESA">
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-control">
+                <input type="text" placeholder="EMAIL">
+            </div>
+            <div class="form-control">
+                <input type="text" placeholder="TELÉFONO">
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-control">
+                <textarea name="" id="" cols="30" rows="10" placeholder="MENSAJE"></textarea>
+            </div>
+        </div>
+        <div class="col">
+            <button type="submit"><span>Enviar</span></button>
+        </div>
+    </div>
+</div>
 <?php get_footer();?>
