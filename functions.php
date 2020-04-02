3308b09895666b40fa8ffa02ab4f28fd05cae191
Byron-Guevara <byronguevara@akevia.mx>
2020-04-02 16:08:40 -0600

    Servicios Avance

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ function degasa_setup_custom_logo_setup() {
     'header-text' => array( 'site-title', 'site-description' ),
     );
     add_theme_support( 'custom-logo', $defaults );
-   }
+    }
 add_action( 'after_setup_theme', 'degasa_setup_custom_logo_setup' );
 
 function add_theme_scripts() {
@@ -38,8 +38,16 @@ function add_theme_scripts() {
 
     wp_enqueue_style( 'generals', get_template_directory_uri() . '/css/generals.css', array(), filemtime( get_stylesheet_directory() . '/css/generals.css' ), 'all');
 
+    wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', array(), null, true);
+
     wp_enqueue_style( 'owl.carousel.min', get_template_directory_uri() . '/css/owl.carousel.min.css', array(), '1.1', 'all');
     wp_enqueue_style( 'owl.theme.default.min', get_template_directory_uri() . '/css/owl.theme.default.min.css', array(), '1.1', 'all');
     wp_enqueue_script('owl.carousel.min', get_template_directory_uri().'/js/owl.carousel.min.js', array('jquery'),'1.1', true);
-  }
-  add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+    if(is_page_template('page-servicios.php')){
+        wp_enqueue_style( 'servicios', get_template_directory_uri() . '/css/servicios.css', array(), filemtime( get_stylesheet_directory() . '/css/servicios.css' ), 'all');
+    }
+
+    }
+    add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
---
+++ page-servicios.php
@@ -0,0 +1,223 @@
+<?php
+/**
+* Template Name: Servicios
+* 
+*/
+?>
+
+<?php get_header(); 
+$ID = get_the_ID();
+?>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div class="contenedor-general-servicios">
+
+    <div class="contenedor-general-servicio active">
+    
+        <div class="contenedor-seccion1">
+            <div class="cont-col">
+                <div class="info">
+                    <img src="<?php echo get_template_directory_uri().'/img/logo-serv-constr.png'; ?>" alt="">
+                    <p class="desc">
+                        Nuestra empresa se integra por un grupo interdisciplinario de profesionistas 
+                        que combinan la experiencia y la juventud para poder atender tus proyectos 
+                        en un abanico de posibilidades dentro de la construcción.
+                    </p>
+                </div>
+                <div class="mask" style="background-image: url(<?php echo get_template_directory_uri().'/img/serv-constr.jpg'; ?>);"></div>
+            </div>
+            
+            <div class="cont-col" style="background-image: url(<?php echo get_template_directory_uri().'/img/serv-constr2.jpg'; ?>);">
+
+            </div>
+        </div>            
+        
+        <div class="contenedor-seccion2" style="background-image: url(<?php echo get_template_directory_uri().'/img/serv-constr-fondo.jpg'; ?>);">
+            <div class="cont-seccion2">
+                <div class="cont-col">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri().'/img/serv-constr3.jpg'; ?>" alt="">
+                    </div>
+                </div>
+                <div class="cont-col">
+                    <div class="info">
+
+                        <p><strong>Entre los que destacan:</strong></p>
+
+                        <ul>
+                            <li>Oficinas Corporativas</li>
+                            <li>Centro y locales comerciales</li>
+                            <li>Instituciones Bancarias</li>
+                            <li>Restaurantes</li>
+                            <li>Hoteles y hospitales</li>
+                            <li>Usos Mixtos</li>
+                            <li>Residenciales</li>
+                            <li>Proyectos Llave en Mano</li>
+                            <li>Proyectos Fast Track</li>
+                            <li>Servicios de Mantenimiento en general</li>
+                        </ul>
+
+                        <div class="dec"></div>
+
+                        <p><strong>¿Cómo podemos ayudarte?</strong></p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="contenedor-general-servicio">
+    
+        <div class="contenedor-seccion1">
+            <div class="cont-col">
+                <div class="info">
+                    <img src="<?php echo get_template_directory_uri().'/img/logo-servicios.png'; ?>" alt="">
+                    <p class="desc">
+                    Entendemos que invertir en bienes raíces requiere 
+                    de un gran esfuerzo y sacrificio, es por esto 
+                    que en DEGASA Desarrollos contamos con proyectos 
+                    que son pensados en lograr la mayor rentabilidad a tu inversión bajo los estándares mas altos en calidad y diseño.
+                    </p>
+                </div>
+                <div class="mask" style="background-image: url(<?php echo get_template_directory_uri().'/img/servicios.jpg'; ?>);"></div>
+            </div>
+            
+            <div class="cont-col" style="background-image: url(<?php echo get_template_directory_uri().'/img/servicios2.jpg'; ?>);">
+
+            </div>
+        </div>
+
+        <div class="contenedor-seccion2" style="background-image: url(<?php echo get_template_directory_uri().'/img/servicios-fondo.jpg'; ?>);">
+            <div class="cont-seccion2">
+                <div class="cont-col">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri().'/img/servicios3.jpg'; ?>" alt="">
+                    </div>
+                </div>
+                <div class="cont-col">
+                    <div class="info">
+
+                        <p><strong style="font-size: 32px;">Contamos con <br> un catálogo de proyectos </strong></p>
+
+                        <p>
+                            que son desarrollados y comercializados directamente 
+                            por nuestra empresa bajo los estándares más altos en calidad 
+                            y diseño logrando ofrecer la mayor rentabilidad a tu inversión.
+                        </p>
+
+                        <div class="dec"></div>
+
+                        <p><strong>¿Cómo podemos ayudarte?</strong></p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="contenedor-general-servicio">
+    
+        <div class="contenedor-seccion1">
+            <div class="cont-col">
+                <div class="info">
+                    <img src="<?php echo get_template_directory_uri().'/img/logo-serv-proh.png'; ?>" alt="">
+                    <p class="desc">
+                        En Pro H nos encargamos de ofrecer asesoría y soluciones constructivas para muros, cielos y fachadas.
+                    </p>
+                </div>
+                <div class="mask" style="background-image: url(<?php echo get_template_directory_uri().'/img/serv-proh.jpg'; ?>);"></div>
+            </div>
+            
+            <div class="cont-col" style="background-image: url(<?php echo get_template_directory_uri().'/img/serv-proh2.jpg'; ?>);">
+
+            </div>
+        </div>
+
+        <div class="contenedor-seccion2" style="background-image: url(<?php echo get_template_directory_uri().'/img/serv-proh-fondo.jpg'; ?>);">
+            <div class="cont-seccion2">
+                <div class="cont-col">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri().'/img/serv-proh3.jpg'; ?>" alt="">
+                    </div>
+                </div>
+                <div class="cont-col">
+                    <div class="info">
+
+                        <p><strong> Mediante la comercialización </strong></p>
+
+                        <p>
+                            de paneles prefabricados ligeros de la marca Panel Rey®
+                            al mejor precio del mercado y asegurando óptimos tiempos de entrega.
+                        </p>
+
+                        <div class="dec"></div>
+
+                        <p><strong>¿Cómo podemos ayudarte?</strong></p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="contenedor-tabs-servicios">
+        <div class="cont-tab active">
+            <img src="<?php echo get_template_directory_uri().'/img/logo-serv-constr-tab.png'; ?>" alt="">
+            <div class="mask" style="background-image: url(<?php echo get_template_directory_uri().'/img/serv-constr.jpg'; ?>);"></div>
+        </div>
+        <div class="cont-tab">
+            <img src="<?php echo get_template_directory_uri().'/img/logo-servicios.png'; ?>" alt="">
+            <div class="mask" style="background-image: url(<?php echo get_template_directory_uri().'/img/servicios.jpg'; ?>);"></div>
+        </div>
+        <div class="cont-tab">
+            <img src="<?php echo get_template_directory_uri().'/img/logo-serv-proh.png'; ?>" alt="">
+            <div class="mask" style="background-image: url(<?php echo get_template_directory_uri().'/img/serv-proh.jpg'; ?>);"></div>
+        </div>
+    </div>
+
+
+</div>
+
+
+<?php get_footer(); ?>
+
+
+<script>
+$(document).ready(function(){
+    $(".contenedor-general-servicios .contenedor-tabs-servicios .cont-tab").each(function(index){
+        $(this).click(function(){
+            $(".contenedor-general-servicios .contenedor-tabs-servicios .cont-tab").removeClass("active");
+            $(this).addClass("active");
+            $(".contenedor-general-servicios .contenedor-general-servicio").removeClass("active");
+            $(".contenedor-general-servicios .contenedor-general-servicio").eq(index).addClass("active");
+        });
+    });
+});
+
+</script>
